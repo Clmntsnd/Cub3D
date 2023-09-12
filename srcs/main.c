950685c423aab9739a541455d7824cf62160d589
Clmntsnd <clement.senand@gmail.com>
2023-09-12 10:38:37 -0400

    Parsing updated, changing priorities

--- srcs/main.c
+++ srcs/main.c
@@ -55,12 +55,12 @@ bool	ft_parse_arg(int ac, char **av)
 	len = ft_strlen(av[1]);
 	if (ac != 2)
 		return (printf("❌ Error\n%s\n%s\n", ERR_ARG, ERR_USG), false);
-	if (len < 5)
-		return (printf("❌ Error\n%s\n%s\n", ERR_NAME, ERR_USG), false);
-    if (ext == NULL || ft_strncmp(ext, ".cub", 5) != 0)
-        return (printf("❌ Error\n%s\n%s\n", ERR_EXT, ERR_USG), false);
 	if (open(av[1], O_RDWR, 0644) == -1)
 		return (printf("❌ Error\n%s\n%s\n", ERR_FILE, ERR_USG), false);
+    if (ext == NULL || ft_strncmp(ext, ".cub", 5) != 0)
+        return (printf("❌ Error\n%s\n%s\n", ERR_EXT, ERR_USG), false);
+	if (len < 5)
+		return (printf("❌ Error\n%s\n%s\n", ERR_NAME, ERR_USG), false);
     return (true);
 }
 
@@ -98,6 +98,6 @@ int	main(int ac, char **av)
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	// Test End
-	
+
 	return (0);
 }
