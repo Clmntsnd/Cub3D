41e87350ae5f93dfc47377d5d8024361c2231df8
Clmntsnd <clement.senand@gmail.com>
2023-09-27 15:20:20 -0400

    added: hit_box wall distance

--- srcs/hooks2.c
+++ srcs/hooks2.c
@@ -206,23 +206,37 @@ void	rotate_vector(double *x, double *y, double angle)
 
 void	move_player(t_ms *ms, double move_speed) 
 {
+	double radius;
+	double hit_box;
+
+	radius = 0.3;
+	if (move_speed < 0)
+		radius = -0.3;
+	hit_box = move_speed + radius;
     // Move along X direction
-    if(ms->map[(int)(ms->game->pl_pos.x + ms->game->pl_dir.x * move_speed)][(int)ms->game->pl_pos.y] == '0')
+    if(ms->map[(int)(ms->game->pl_pos.x + ms->game->pl_dir.x * hit_box)][(int)ms->game->pl_pos.y] == '0')
         ms->game->pl_pos.x += ms->game->pl_dir.x * move_speed;
     
     // Move along Y direction
-    if(ms->map[(int)ms->game->pl_pos.x][(int)(ms->game->pl_pos.y + ms->game->pl_dir.y * move_speed)] == '0')
+    if(ms->map[(int)ms->game->pl_pos.x][(int)(ms->game->pl_pos.y + ms->game->pl_dir.y * hit_box)] == '0')
         ms->game->pl_pos.y += ms->game->pl_dir.y * move_speed;
 }
 
 void strafe_player(t_ms *ms, double strafe_speed) 
 {
+	double radius;
+	double hit_box;
+
+	radius = 0.3;
+	if (strafe_speed < 0)
+		radius = -0.3;
+	hit_box = strafe_speed + radius;
     // Strafe along X direction (perpendicular to direction of facing, so we use pl_dir.y)
-    if(ms->map[(int)(ms->game->pl_pos.x + ms->game->pl_dir.y * strafe_speed)][(int)ms->game->pl_pos.y] == '0')
+    if(ms->map[(int)(ms->game->pl_pos.x + ms->game->pl_dir.y * hit_box)][(int)ms->game->pl_pos.y] == '0')
         ms->game->pl_pos.x += ms->game->pl_dir.y * strafe_speed;
     
     // Strafe along Y direction (perpendicular to direction of facing, so we use -pl_dir.x)
-    if(ms->map[(int)ms->game->pl_pos.x][(int)(ms->game->pl_pos.y - ms->game->pl_dir.x * strafe_speed)] == '0')
+    if(ms->map[(int)ms->game->pl_pos.x][(int)(ms->game->pl_pos.y - ms->game->pl_dir.x * hit_box)] == '0')
         ms->game->pl_pos.y -= ms->game->pl_dir.x * strafe_speed;
 }
 
