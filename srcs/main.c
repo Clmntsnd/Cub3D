5ee82297e7fb8550b384337182fe2bb2214f5d38
Clmntsnd <clement.senand@gmail.com>
2023-10-05 13:20:50 -0400

    Cleaning main and add comments

--- srcs/main.c
+++ srcs/main.c
@@ -1,27 +1,5 @@
 #include "../includes/cub3d.h"
 
-void	print_init(t_ms *ms)
-{
-	printf("\nplpos.x: 	%.2f\n", ms->game->pl_pos.x);
-	printf("plpos.y: 	%.2f\n", ms->game->pl_pos.y);
-	printf("pldir.x: 	%.2f\n", ms->game->pl_dir.x);
-	printf("pldir.y: 	%.2f\n", ms->game->pl_dir.y);
-	printf("plane.x: 	%.2f\n", ms->game->plane.x);
-	printf("plane.y: 	%.2f\n", ms->game->plane.y);
-	printf("cam_x: 		%.2f\n", ms->game->cam_x);
-	printf("ray_dir.x: 	%.2f\n", ms->game->ray_dir.x);
-	printf("ray_dir.y: 	%.2f\n", ms->game->ray_dir.y);
-	// printf("map.x: 		%.2f\n", ms->game->map.x);
-	// printf("map.y: 		%.2f\n", ms->game->map.y);
-	printf("side_dist.x: 	%.2f\n", ms->game->side_dist.x);
-	printf("side_dist.y: 	%.2f\n", ms->game->side_dist.y);
-	printf("delta_dist.x: 	%.2f\n", ms->game->delta_dist.x);
-	printf("delta_dist.y: 	%.2f\n", ms->game->delta_dist.y);
-	printf("step.x: 	%.2f\n", ms->game->step.x);
-	printf("step.y: 	%.2f\n", ms->game->step.y);
-	printf("side:		%d\n", ms->game->side);
-}
-
 int	init_mlx(t_ms *ms)
 {
 	if (!(ms->mlx = mlx_init(WIDTH, HEIGHT, "Cub3d", true)))
@@ -54,13 +32,17 @@ int	main(int ac, char **av)
 		return(EXIT_FAILURE);
 
 	init_content(ms, av[1]);
+
+	//TODO get_texture needs rework
 	// if(!get_texture(ms))
 	// 	return (EXIT_FAILURE);
+
 	if(!get_color(ms))
         return (EXIT_FAILURE);
 	if (init_mlx(ms) == 1)
 		return(EXIT_FAILURE);
 
+	//TODO put this in a function
 	//grab the texture from the path
 	ms->tex = ft_calloc(1, sizeof(t_tex));
 	if (!ms->tex)
@@ -84,16 +66,15 @@ int	main(int ac, char **av)
 	ms->tex->no = fill_texture(ms->tex->no_tex);
 	ms->tex->we = fill_texture(ms->tex->we_tex);
 	ms->tex->ea = fill_texture(ms->tex->ea_tex);
-
+	//TODO put the above in a function
+	
 	mlx_loop_hook(ms->mlx, loop, ms->mlx);
-	// mlx_cursor_hook(ms->mlx, &move_cursor, ms->mlx);
 	
 	// Function that draws the minimap
 	// if any init doesn't work, returns an error
 	// if (draw_map2D(ms) != 0)
 	// 	return(EXIT_FAILURE);
 
-	// mlx_delete_image(ms->mlx, ms->m_img);
 	mlx_loop(ms->mlx);
 	mlx_terminate(ms->mlx);
 	//TODO don't forget to free shit
