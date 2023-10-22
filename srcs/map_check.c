e464915dee816c352d89b6ad10b913a850c9e13d
Clmntsnd <clement.senand@gmail.com>
2023-10-22 18:32:36 -0400

    fix: test passed on all maps (good and bad), floodfill fixed

--- srcs/map_check.c
+++ srcs/map_check.c
@@ -63,35 +63,46 @@ void	check_arg_dup(t_ms *ms)
 	free(flag);
 }
 
-int	floodfill(char **map, char v, int x, int y)
-{
-	while (map[x][y] == '0')
-	{
-		map[x][y] = v;
-		floodfill(map, v, x + 1, y);
-		floodfill(map, v, x - 1, y);
-		floodfill(map, v, x, y - 1);
-		floodfill(map, v, x, y + 1);
-	}
-	if (map[x][y] == ' ' || map[x][y] == '\t' || map[x][0] == '0'
-		|| (map[x][y + 1] == '\n' && map[x][y] == '0') ||
-		(map[x][y] == ' ' && map[x][y + 1] == '\0'))
-		return (1);
-	return (0);
+bool is_valid_position(char **map, int x, int y) {
+    return x >= 0 && y >= 0 && map[x] && map[x][y] && 
+           (map[x][y] == '0' || map[x][y] == 'N' || map[x][y] == 'S' || 
+            map[x][y] == 'E' || map[x][y] == 'W');
 }
 
-int	rev_floodfill(char **map, char v, int x, int y)
+bool has_boundary_violation(char **map, int x, int y) {
+    if (!map[x] || !map[x][y] || map[x][y] == ' ' || map[x][y] == '\t' || 
+        (map[x][y + 1] == '\0' && map[x][y] == '0') || 
+        (map[x][y + 1] == '\n' && map[x][y] == '0')) {
+        return true;
+    }
+    return false;
+}
+
+int floodfill(char **map, char v, int x, int y) {
+    if (has_boundary_violation(map, x, y)) {
+        return 1;
+    }
+    
+    if (is_valid_position(map, x, y) && map[x][y] == '0') {
+        map[x][y] = v;
+        return floodfill(map, v, x + 1, y) ||
+               floodfill(map, v, x - 1, y) ||
+               floodfill(map, v, x, y + 1) ||
+               floodfill(map, v, x, y - 1);
+    }
+    return 0;
+}
+
+int rev_floodfill(char **map, char v, int x, int y) 
 {
-	while (map[x][y] == 'X')
-	{
-		map[x][y] = v;
-		rev_floodfill(map, v, x + 1, y);
-		rev_floodfill(map, v, x - 1, y);
-		rev_floodfill(map, v, x, y - 1);
-		rev_floodfill(map, v, x, y + 1);
-	}
-	if (map[x][y] == ' ' || map[x][y] == '\t' || map[x][0] == '0'
-		|| (map[x][y + 1] == '\n' && map[x][y] == '0'))
-		return (1);
-	return (0);
+    if (has_boundary_violation(map, x, y))
+        return 1;
+    if (map[x][y] == 'X') {
+        map[x][y] = v;
+        return rev_floodfill(map, v, x + 1, y) ||
+               rev_floodfill(map, v, x - 1, y) ||
+               rev_floodfill(map, v, x, y + 1) ||
+               rev_floodfill(map, v, x, y - 1);
+    }
+    return 0;
 }
